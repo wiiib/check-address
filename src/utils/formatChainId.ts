ce8bdd578e190644d08dc8f8ef289b85b1523e62
pyncz <pavelyanycz@gmail.com>
2023-02-17 16:18:42 +0400

    fix: Handle `0` passed as the ID in the `formatChainId` util

--- src/utils/formatChainId.ts
+++ src/utils/formatChainId.ts
@@ -2,17 +2,12 @@ import type { Optional } from '@voire/type-utils'
 import type { ChainID } from '../models'
 
 export const formatChainId = (id: ChainID) => {
-  let intId: Optional<number>
+  const intId: Optional<number> = +id
+    ? +id
+    : undefined // NaN, 0 etc
 
-  if (typeof id === 'number') {
-    intId = id
-  } else {
-    const numericId = +id
-    if (isNaN(numericId)) {
-      throw new TypeError(`Invalid ID! The value "${id}" cannot be interpreted as a chain ID`)
-    } else {
-      intId = numericId
-    }
+  if (!intId || intId < 1) {
+    throw new TypeError(`Invalid ID! The string "${id}" cannot be interpreted as a chain ID`)
   }
 
   const hexId = intId ? `0x${intId.toString(16)}` : undefined
